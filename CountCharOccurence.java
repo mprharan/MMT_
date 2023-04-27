a0fa7bb6b097790cbf224853a9734c0659050af9
Hariharan <131951957+mprharan@users.noreply.github.com>
2023-04-27 14:05:11 +0530

    Update CountCharOccurence.java

--- CountCharOccurence.java
+++ CountCharOccurence.java
@@ -4,7 +4,7 @@ public class CountCharOccurence {
 	
 	public static void main(String[] args) {
 		
-		String str="Java is object oriented language";
+		 str="Java is object oriented language";
 		
 		int result=str.length()-str.replaceAll("e", "").length();
 		System.out.println(result);
